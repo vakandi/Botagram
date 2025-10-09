c5bae4870b9b96e75ef676b32f9951be2ef35cef
vakandi <wael.bousfira@protonmail.com>
2025-10-09 23:54:29 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     allowedHosts: ["59f9cc8e3c4a.ngrok-free.app", "6545aab9f8c1.ngrok-free.app", "b9897d1dee22.ngrok-free.app", "1df55b40310c.ngrok-free.app", "b6cbf5252a59.ngrok-free.app"],
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
